be320aef790e3ac73f074c591cb1a763b07c037c
Shiv Pratap Singh Yadav <shivpratapsinghyadav38@gmail.com>
2022-12-28 13:17:48 +0530

    n

--- mycrud/src/App.jsx
+++ mycrud/src/App.jsx
@@ -6,7 +6,8 @@ import "./css/main.css";
 function App() {
   return (
     <div className="App">
-      <h1>My Todo</h1>
+      {/* <img style={{marginLeft:"230px",borderRadius:"60%"}} width="20%" src="https://lh3.googleusercontent.com/a/AEdFTp6-2XxSL_K_lXH9NyLZF_WdVPbRmn532wr22bzQ=s288-p-rw-no"/> */}
+      <h1>Todo By Shiv</h1>
       <Todo />
       <DisplayTodo />
     </div>
